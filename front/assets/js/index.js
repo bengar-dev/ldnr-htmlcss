05c307ee48e33b7f05271a919583297317d1db4a
bengar-dev <benoitgar.pro@gmail.com>
2022-10-11 12:05:34 +0200

    Add Studio section

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,28 +1,18 @@
-/*
-Utilisez l'API Fetch pour collecter les données dans le backend.
-Les fichiers à récupérer sont :
-../back/guitars.json
-../back/populars.json
-../back/studio.json
-Les images à insérer sont dans le répertoire ../back/images/,
-vous trouverez leurs noms et descriptions dans les JSON téléchargés.
-*/
-/* Exemple de code d'utilisation de fetch :
-*/
-fetch("../back/guitars.json")
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
+async function getStudio() {
+  try {
+    const request = await fetch("../../back/studio.json");
+    const response = await request.json();
+    if (response.length > 0) {
+      for (let i = 0; i < response.length; i++) {
+        document.querySelector(`#studio-${i}`).innerHTML = `
+          <img src="../back/images/${response[i].imageUrl}" class="stretch" />
+          <h3 class="text_studio">${response[i].title}</h3>
+          `;
+      }
     }
-  })
-  .then(data => {
-    // Utilisation des données dé-jsonifiées dans l'objet data
-    console.table(data);
-    // Regardez la structure du json, comprenez son contenu, intégrez-le
-    // dans votre HTML.
-  })
-  .catch(function(error) {
-    // Gestion des erreurs
-    console.error("Something goes wrong!");
-    console.error(error);
-  });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+getStudio();
