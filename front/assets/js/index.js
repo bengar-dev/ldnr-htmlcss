b7936cb9bcba20f77d5ab1dded2a59648f443d34
bengar-dev <benoitgar.pro@gmail.com>
2022-10-11 15:38:14 +0200

    Add main section, most popular, guitares

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,6 +1,9 @@
+const guitaresContent = document.querySelector(".guitares-content");
+const popularContent = document.querySelector(".popular-content");
+
 async function getStudio() {
   try {
-    const request = await fetch("../../back/studio.json");
+    const request = await fetch("../back/studio.json");
     const response = await request.json();
     if (response.length > 0) {
       for (let i = 0; i < response.length; i++) {
@@ -15,4 +18,74 @@ async function getStudio() {
   }
 }
 
+async function getGuitares() {
+  try {
+    const request = await fetch("../back/guitars.json");
+    const response = await request.json();
+    if (response.length > 0) {
+      for (const element of response) {
+        const randomRating = Math.floor(Math.random() * (5 - 1) + 1);
+        guitaresContent.innerHTML += `
+        <article class="guitare-article">
+            <img src="../back/images/${element.imageUrl}" alt="${
+          element.altTxt
+        }"/>
+            <h3>${element.name}</h3>
+            <p>${element.price}€ ou <span class="bold">${
+          element.monthly
+        }€ / mois</span></p>
+            <div class="guitare-rating">
+              ${renderRating(randomRating)}
+            </div>
+          </article>`;
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function getPopular() {
+  try {
+    const request = await fetch("../back/populars.json");
+    const response = await request.json();
+    if (response.length > 0) {
+      for (const element of response) {
+        const randomRating = Math.floor(Math.random() * (5 - 1) + 1);
+        popularContent.innerHTML += `
+        <article class="popular-article">
+            <img src="../back/images/${element.imageUrl}" alt="${
+          element.altTxt
+        }"/>
+            <div class="popular-article-content">
+              <h3>${element.name}</h3>
+              <p>${element.price}€ ou <span class="bold">${
+          element.monthly
+        }€ / mois</span></p>
+              <div class="popular-article-rating">
+                ${renderRating(randomRating)}
+              </div>
+            </div>
+          </article>`;
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+function renderRating(number) {
+  let ratings = "";
+  const restRating = 5 - number;
+  for (let i = 0; i < number; i++) {
+    ratings += `<i class="fas fa-star primary-color"></i>`;
+  }
+  for (let i = 0; i < restRating; i++) {
+    ratings += `<i class="fas fa-star border-color"></i>`;
+  }
+  return ratings;
+}
+
+getGuitares();
+getPopular();
 getStudio();
