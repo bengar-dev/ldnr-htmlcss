bdae5265f95d60a98684e659f7434c71324b064f
bengar-dev <benoitgar.pro@gmail.com>
2022-10-14 16:03:25 +0200

    Update fix

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,40 +1,22 @@
 const guitaresContent = document.querySelector(".guitares-content");
 const popularContent = document.querySelector(".popular-content");
 
-(async function getStudio() {
-  try {
-    const request = await fetch("../back/studio.json");
-    const response = await request.json();
-    if (response.length > 0) {
-      for (let i = 0; i < response.length; i++) {
-        document.querySelector(`#studio-${i}`).innerHTML = `
-          <img src="../back/images/${response[i].imageUrl}" class="stretch" />
-          <h3 class="text_studio">${response[i].title}</h3>
-          `;
-      }
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
-})();
-
 (async function getGuitares() {
   try {
     const request = await fetch("../back/guitars.json");
     const response = await request.json();
-    if (response.length > 0) {
+    if (Array.isArray(response) && response.length > 0) {
       for (const element of response) {
         const randomRating = Math.floor(Math.random() * (5 - 1) + 1);
         guitaresContent.innerHTML += `
         <article class="guitare-article">
-            <img src="../back/images/${element.imageUrl}" alt="${element.altTxt
-          }"/>
-            <h3>${element.name}</h3>
-            <p>${element.price}€ ou <span class="bold">${element.monthly
+        <img src="../back/images/${element.imageUrl}" alt="${element.altTxt}"/>
+        <h3>${element.name}</h3>
+        <p>${element.price}€ ou <span class="bold">${element.monthly
           }€ / mois</span></p>
-            <div class="guitare-rating">
-              ${renderRating(randomRating)}
-            </div>
+        <div class="guitare-rating">
+        ${renderRating(randomRating)}
+        </div>
           </article>`;
       }
     }
@@ -47,22 +29,21 @@ const popularContent = document.querySelector(".popular-content");
   try {
     const request = await fetch("../back/populars.json");
     const response = await request.json();
-    if (response.length > 0) {
+    if (Array.isArray(response) && response.length > 0) {
       for (const element of response) {
         const randomRating = Math.floor(Math.random() * (5 - 1) + 1);
         popularContent.innerHTML += `
         <article class="popular-article">
-            <img src="../back/images/${element.imageUrl}" alt="${element.altTxt
-          }"/>
-            <div class="popular-article-content">
-              <h3>${element.name}</h3>
+        <img src="../back/images/${element.imageUrl}" alt="${element.altTxt}"/>
+        <div class="popular-article-content">
+        <h3>${element.name}</h3>
               <p>${element.price}€ ou <span class="bold">${element.monthly
           }€ / mois</span></p>
               <div class="popular-article-rating">
-                ${renderRating(randomRating)}
+              ${renderRating(randomRating)}
               </div>
-            </div>
-          </article>`;
+              </div>
+              </article>`;
       }
     }
   } catch (err) {
@@ -70,6 +51,27 @@ const popularContent = document.querySelector(".popular-content");
   }
 })();
 
+(async function getStudio() {
+  try {
+    const request = await fetch("../back/studio.json");
+    const response = await request.json();
+    if (Array.isArray(response) && response.length > 0) {
+      for (let i = 0; i < response.length; i++) {
+        document.querySelector(`#studio-${i}`).innerHTML = `
+          <img src="../back/images/${response[i].imageUrl}" class="stretch" />
+          <h3 class="text_studio">${response[i].title}</h3>
+          `;
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
+
+/**
+ * @param {number} number
+ * @returns string
+ */
 function renderRating(number) {
   let ratings = "";
   const restRating = 5 - number;
